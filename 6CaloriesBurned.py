20b035cc1362d61fd33b9e4c356793e83f73e34b
OnlyNoodles <balakrishn4@middleton.school.nz>
2024-02-11 23:10:57 +1300

    mm

---
+++ 6CaloriesBurned.py
@@ -0,0 +1,11 @@
+def workout():
+    bicycling = float(input("Enter the amount of hours you spent bicycling: "))
+    jogging = float(input("Enter the amount of hours spent jogging: "))
+    swimming = float(input("Enter the amount of hours you spent swimming: "))
+    calories = (bicycling * 200 + jogging * 475 + swimming * 275)
+    kilograms = calories / 3500 * 454 / 1000
+    print(f"You have lost {kilograms} kilograms.")
+
+
+if __name__ == "__main__":
+    workout()
